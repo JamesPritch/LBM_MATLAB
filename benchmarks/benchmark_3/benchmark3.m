63edc7a234a2402594cb74c3b75910437f0b967d
james0pritchard <james.pritch@icloud.com>
2023-12-01 21:37:15 +0000

    Benchmark 3 implementation
    I modified my Benchmark 2 script to include various boundary conditions to suit the third benchmark problem. I also changed the order of nx,ny to ny,nx due to MATLAB's nature of computing columns first. This works but is extremely slow, it took more iterations than I have set niter to, which I haven't quite tuned to get an accurate plot yet but I have used around of 25000 and it's not quite there.

---
+++ benchmarks/benchmark_3/benchmark3.m
@@ -0,0 +1,123 @@
+%% Lattice Boltzmann method code: advection-diffusion of Cylinder without flow
+%% Setting Grid Independant Variables
+% Simulation parameters - input
+% Scalars
+C_p = 1;
+u_0 = 0.05;
+nx = 1600;
+ny = 160;
+niter = 20000; % 15000 took my computer over 28 minutes
+
+% Vectors
+x = 1:nx;
+y = 1:ny;
+
+% D2Q9 velocity set parameters
+ndir = 9;
+cssq = 1/3;
+cx = [0, 1, -1, 0, 0, 1, -1, -1, 1];
+cy = [0, 0, 0, 1, -1, 1, 1, -1, -1];
+w = [4/9, 1/9, 1/9, 1/9, 1/9, 1/36, 1/36, 1/36, 1/36];
+
+% Simulation parameters - built
+cssqinv = 1/cssq;
+omega = 1.38; % 1/tau;
+omomega = 1 - omega;
+D = 0.07407; % cssq*(tau - 0.5);
+
+% Initialisation of the concentration and velocity field at time t = 0
+C = zeros(ny, nx);
+C(1,:) = C_p;
+
+ux = zeros(ny, nx) + u_0;
+uy = zeros(ny, nx);
+
+%% Simulating using LBM
+% Initialisation of the particle distribution function
+geq = zeros(ny, nx, ndir);
+for k = 1:ndir
+    cdotu = cx(k)*ux + cy(k)*uy;
+    udotu = ux.^2 + uy.^2;
+    geq(:, :, k) = w(k)*C.*(1 + cssqinv*cdotu + 0.5*cssqinv^2*cdotu.^2 - 0.5*cssqinv*udotu);
+end
+g = geq;
+gcol = zeros(ny, nx, ndir);
+
+% Simulation loop
+fprintf('Starting simulation \n');
+tic
+for t = 1:niter
+    % Collision
+    gcol = omomega*g + omega*geq;
+
+    % Streaming - Explicit version
+    % This streaming implementation automatically applies periodic boundary
+    % conditions in all edges of the computational domain.
+    for k = 1:ndir
+        for i = 1:ny
+            for j = 1:nx
+                xstreamed = mod(j + cx(k), nx);
+                if xstreamed == 0
+                    xstreamed = nx;
+                end
+                ystreamed = mod(i + cy(k), ny);
+                if ystreamed == 0
+                    ystreamed = ny;
+                end
+                g(ystreamed, xstreamed, k) = gcol(i, j, k);
+            end
+        end
+    end
+
+    % Boundary conditions
+    % LHS Boundary ie C = 0
+    for i = 1:ny
+        g(i,1,:) = 0;
+    end
+    % RHS Boundary ie partial C wrt x = 0
+    for i = 1:ny
+        g(i,nx,:) = g(i,nx-1,:);
+    end
+    % Top Boundary ie partial C wrt y = 0
+    for i = 1:nx
+        g(ny,i,:) = g(ny-1,i,:);
+    end
+    % Bottom Boundary ie Anti-BB
+    opp = [0 2 1 4 3 7 8 5 6];
+    for i = 1
+        for j = 1:nx
+            for k = 1:ndir
+                g(i,j,opp(k)+1) = -gcol(i,j,k) + 2*w(k) * C_p;
+            end
+        end
+    end
+
+    % Macroscopic variables
+    C = g(:, :, 1) + g(:, :, 2) + g(:, :, 3) + g(:, :, 4) + g(:, :, 5)...
+        + g(:, :, 6) + g(:, :, 7) + g(:, :, 8) + g(:, :, 9);
+
+    % Equilibrium distribution function
+    for k = 1:ndir
+        cdotu = cx(k)*ux + cy(k)*uy;
+        udotu = ux.^2 + uy.^2;
+        geq(:, :, k) = w(k)*C.*(1 + cssqinv*cdotu + 0.5*cssqinv^2*cdotu.^2 - 0.5*cssqinv*udotu);
+    end
+
+    if mod(t, 10) == 0
+        fprintf('Iteration: %d, Time: %f \n', t, toc);
+    end
+
+end
+
+
+%% Saving C to file:
+save /Users/jpritch/Documents/MATLAB/benchmarks/benchmark_3/C C
+
+
+
+
+
+
+
+
+
---
+++ benchmarks/benchmark_3/benchmark3_plot.m
@@ -0,0 +1,55 @@
+%% Plotting of Benchamrk 1 with different grid sizes
+%% Read C_BGK from file
+load /Users/jpritch/Documents/MATLAB/benchmarks/benchmark_3/C
+C_BGK = C;
+
+
+%% Setting Variables
+% Scalars
+C_p = 1;
+D = 0.07407;
+u_0 = 0.05;
+nx = 1600;
+ny = 160;
+
+% Vectors
+x = (1:nx);
+y = (1:ny);
+C_exact = zeros(ny,nx);
+
+
+%% Finding Analytical Solution for C
+for i = 1:ny
+    for j = 1:nx
+        C_exact(i,j) = C_p * (1-erf(y(i)/sqrt(4*D*x(j)/u_0))) ;
+    end
+end
+
+
+%% Plotting C contours
+% Plotting C_exact and C_BGK Concentration contours 
+figure;
+axes('FontSize',16, 'NextPlot', 'add');
+plot(1:2,1:2,'k');
+plot(1:2,1:2,'k--');
+contour(x/ny,y/ny,C_exact, [0.2 0.4 0.6 0.8], 'k', LineWidth=0.7)
+[C,h] = contour(x/ny,y/ny,C_BGK, [0.2 0.4 0.6 0.8], 'k--', LineWidth=0.7);
+clabel(C,h,'FontSize',18, 'LabelSpacing',2000)
+xlabel('${x/N_y}$','interpreter','latex', fontsize=26) 
+ylabel('${y/N_y}$','interpreter','latex', fontsize=26)
+% axis want not equal
+xlim([0 10])
+ylim([0 1])
+leg = legend({'Analytical', 'BGK', '', ''}, 'interpreter','latex', ...
+    fontsize = 18);
+leg.ItemTokenSize = [25,25,25];
+
+% Plotting C_BGK
+% figure;
+% contour(C_BGK)
+
+
+
+
+
+
